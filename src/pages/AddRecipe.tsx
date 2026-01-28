b26d90300e7dc9d4f8326a8546807e3bae257442
khushi05sharma <5103.sharmakhushi@gmail.com>
2026-01-29 01:06:55 +0530

    added add recipe

---
+++ src/pages/AddRecipe.tsx
@@ -0,0 +1,12 @@
+function AddRecipe() {
+  return (
+    <div className="min-h-screen bg-primary-light">
+      <h1 className="text-4xl font-bold text-center pt-20">Add recipe page</h1>
+      <p className="text-center mt-4 text-gray-600">
+        This will have a form to add new recipes
+      </p>
+    </div>
+  );
+}
+
+export default AddRecipe();
