4621df81f17183d0eb8e74badf0c39022fa06c8a
khushi05sharma <5103.sharmakhushi@gmail.com>
2026-01-29 01:11:12 +0530

    added login form

---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+function Login() {
+  return (
+    <div className="min-h-screen bg-primary-light">
+      <h1 className="text-4xl font-bold text-center pt-20">Login Page</h1>
+      <p className="text-center mt-4 text-gray-600">
+        This will have login form
+      </p>
+    </div>
+  );
+}
+
+export default Login();
