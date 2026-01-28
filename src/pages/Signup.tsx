65787f40630fcf2ad04ee96308478c87296fc0ae
khushi05sharma <5103.sharmakhushi@gmail.com>
2026-01-29 01:11:58 +0530

    added signup form

---
+++ src/pages/Signup.tsx
@@ -0,0 +1,12 @@
+function Signup() {
+  return (
+    <div className="min-h-screen bg-primary-light">
+      <h1 className="text-4xl font-bold text-center pt-20">Signup Page</h1>
+      <p className="text-center mt-4 text-gray-600">
+        This will have signup form
+      </p>
+    </div>
+  );
+}
+
+export default Signup();
