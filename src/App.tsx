d2f230e748ddb9c4a024eef0f3ed99543709a309
khushi05sharma <5103.sharmakhushi@gmail.com>
2026-04-08 23:43:11 +0530

    added forgot and reset password in app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 // Import routing components from react-router-dom
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Layout from './components/layout/Layout';
-import TestComponents from './pages/TestComponents';
-import TestRecipeCard from './pages/TestRecipeCard';
+import Layout from "./components/layout/Layout";
+import TestComponents from "./pages/TestComponents";
+import TestRecipeCard from "./pages/TestRecipeCard";
 
 //Import all page components
 import Home from "./pages/Home";
@@ -13,27 +13,29 @@ import Notifications from "./pages/Notifications";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import RecipeDetail from "./pages/RecipeDetail";
-
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
   return (
     <BrowserRouter>
       {/* React Router will match the current URL to ONE of these routes */}
       <Layout>
-      <Routes>
-        {/* element prop specifies which component to render */}
-        <Route path="/" element={<Home />} />
-        {/* :id is a URL parameter (dynamic segment) */}
-        <Route path="/recipe/:id" element={<RecipeDetail />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/add-recipe" element={<AddRecipe />} />
-        <Route path="/notifications" element={<Notifications />} />
-        <Route path="/test" element={<TestComponents />} />
-        <Route path="/test-card" element={<TestRecipeCard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        
-      </Routes>
+        <Routes>
+          {/* element prop specifies which component to render */}
+          <Route path="/" element={<Home />} />
+          {/* :id is a URL parameter (dynamic segment) */}
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/add-recipe" element={<AddRecipe />} />
+          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/test" element={<TestComponents />} />
+          <Route path="/test-card" element={<TestRecipeCard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+        </Routes>
       </Layout>
     </BrowserRouter>
   );
